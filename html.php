2d82e0a9fbbd5514b12f84b417815fedd1d97619
tmalaspina <126913332+tmalaspina@users.noreply.github.com>
2026-04-12 12:42:03 +0200

    Enhance documentation for html class
    Added PHPDoc comments to the html class and its methods for better documentation.

--- html.php
+++ html.php
@@ -1,16 +1,47 @@
 <?php
- class html {
+
+/**
+ * Classe base per la generazione di HTML.
+ *
+ * Definisce l'interfaccia comune a tutti gli elementi della libreria objectHtml.
+ * Le classi figlie devono implementare il metodo {@see build()} per popolare
+ * la proprietà {@see $html} con il markup generato.
+ *
+ * @package objectHtml
+ */
+class html {
+
+	/**
+	 * Stringa contenente l'HTML generato da {@see build()}.
+	 *
+	 * Viene popolata dalle classi figlie all'interno del metodo {@see build()}.
+	 *
+	 * @var string|null
+	 */
 	protected $html;
 
-	
-	function get():string {
+	/**
+	 * Esegue la build e restituisce l'HTML generato come stringa.
+	 *
+	 * Invoca {@see build()} per assicurarsi che {@see $html} sia aggiornato,
+	 * poi restituisce il risultato. Se {@see $html} è null, restituisce
+	 * una stringa vuota.
+	 *
+	 * @return string L'HTML generato, oppure una stringa vuota.
+	 */
+	function get(): string {
 		$this->build();
 		return is_null($this->html) ? "" : $this->html;
 	}
 
+	/**
+	 * Stampa in output l'HTML generato.
+	 *
+	 * Equivale a fare `echo $this->get()`.
+	 *
+	 * @return void
+	 */
 	function show() {
-//		$this->build();
-//		var_dump($this->html);
 		echo $this->get();
 	}
 }
